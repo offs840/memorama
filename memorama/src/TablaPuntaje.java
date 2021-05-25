7254e844b1ce329cf3512d41dd08376cf78a8a87
Pablo Rios <pablorios1603@gmail.com>
2021-05-24 21:55:46 -0500

    Feat: Creación de los archivos

--- memorama/src/TablaPuntaje.java
+++ memorama/src/TablaPuntaje.java
@@ -1,13 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
+import java.io.File;
 
 /**
  *
  * @author 
  */
 public class TablaPuntaje {
+    private String datos;
+    private File archivo;
+    
+    public TablaPuntaje() {
+        try {
+            String ruta = "registros/puntajes.txt";            
+            File file = new File(ruta);            
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+
+
+    public void setDatos(String datos) {
+        this.datos = datos;
+    }
     
 }
+
+
